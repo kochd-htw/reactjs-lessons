42576fd1955d3c226e5518496ee3911dc1d6071b
Denny <166122821+kochd-htw@users.noreply.github.com>
2024-05-02 21:56:52 +0200

    side effects example

--- src/react/Home.jsx
+++ src/react/Home.jsx
@@ -1,17 +1,19 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
-import catNames from './catNames'
+import getRandomCatName from './catNames'
 
 import Image from './Image'
 
 const Home = () => {
   /*
-    Components can be thought of as state-machines.
-    They hold a state and change their behaviour and render-output depending on it.
-    In order for your component to re-render after a state change, you must use a
-    react hook called useState
+    State in components needs to be synchronous.
+    It is only allowed to change though props or via the useState hook.
+    This way the react framework can predictably determine the state of a component and decide if the component
+    needs to be re-rendered in the next render-cycle.
+    Because we can't predict the timing of state changes due to things like events, user-inputs or calls to remote services,
+    these so called side-effects need to be handled differently.
   */
-  const [catName, setCatName] = useState(catNames())
+  const [catName, setCatName] = useState(getRandomCatName())
 
   const catIds = [
     'FmTkbYVy0FSi4QBl',
@@ -20,8 +22,23 @@ const Home = () => {
     '2uWNSTyOg2IVBcAL'
   ]
 
+  // Start an asynchronous side-effect via useEffect hook.
+  useEffect(() => {
+    // This runs one time at the mounting (first creation) of the component.
+    // It sets up an interval that changes the catName state every second.
+    const interval = setInterval(() => setCatName(getRandomCatName()), 1000)
+
+    // If we return a function from useEffect. Is will be called if the component is being un-mounted (destroyed).
+    // This is useful to clean up listeners or cancel remote calls or timers.
+    return () => clearInterval(interval)
+
+    // The second parameter of useEffect is it's "dependency array".
+    // The dependency array tells react when to re-execute the effect.
+    // If it is empty, the effect only fires ones at component mounting.
+  }, [])
+
   const handleChangeCOTD = () => {
-    setCatName(catNames())
+    setCatName(getRandomCatName())
   }
 
   return (
