58b1e5d7688d404c9252ae77e0013a77d4e69806
Denny Koch <kochd@ht-berlin.de>
2026-07-03 12:27:10 +0200

    trying to fix babel import

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import React, { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
+
+const App = () => (
+  <View style={styles.container}>
+    <Text>Open up App.js to start working on your app!</Text>
+    <StatusBar style="auto" />
+  </View>
+)
+
+export default App
